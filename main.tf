6abf9bccf7ad3d8f0c0ddb9e7b57586fa5b890c2
Shauny123 <bywordofmouthcatering@gmail.com>
2025-07-26 11:24:34 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,53 @@
-terraform {
-  required_providers {
-    google = { source = "hashicorp/google", version = "~> 5.0" }
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+resource "google_compute_network" "vpc_network" {
+  name                    = "${var.environment}-vpc"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "subnet" {
+  name          = "${var.environment}-subnet"
+  ip_cidr_range = var.subnet_cidr
+  region        = var.region
+  network       = google_compute_network.vpc_network.id
+}
+
+resource "google_container_cluster" "primary" {
+  name     = "${var.environment}-gke"
+  location = var.region
+
+  networking_mode = "VPC_NATIVE"
+  network         = google_compute_network.vpc_network.id
+  subnetwork      = google_compute_subnetwork.subnet.id
+
+  ip_allocation_policy {
+    cluster_secondary_range_name  = "pods"
+    services_secondary_range_name = "services"
+  }
+
+  master_authorized_networks_config {
+    cidr_blocks {
+      cidr_block   = var.master_cidr
+      display_name = "admin"
+    }
+  }
+
+  remove_default_node_pool = true
+  initial_node_count       = 1
+}
+
+resource "google_container_node_pool" "primary_nodes" {
+  name       = "${var.environment}-nodes"
+  location   = var.region
+  cluster    = google_container_cluster.primary.name
+  node_count = 1
+
+  node_config {
+    machine_type = "e2-medium"
+    oauth_scopes = ["https://www.googleapis.com/auth/cloud-platform"]
   }
 }
 
-variable "project_id" {}
-variable "environment" {}
-resource "google_container_cluster" "primary" {
-  name     = "byword-${var.environment}"
-  location = "us-central1"
-}
