2dcf8c7ae443d28f668956fbd4d4e53fe635bacc
Shauny123 <bywordofmouthcatering@gmail.com>
2025-07-26 12:23:09 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,8 +4,7 @@ provider "google" {
 }
 
 resource "google_compute_network" "vpc_network" {
-  name                    = "${var.environment}-vpc"
-  auto_create_subnetworks = false
+  name = "${var.environment}-vpc"
 }
 
 resource "google_compute_subnetwork" "subnet" {
@@ -15,39 +14,38 @@ resource "google_compute_subnetwork" "subnet" {
   network       = google_compute_network.vpc_network.id
 }
 
-resource "google_container_cluster" "primary" {
-  name     = "${var.environment}-gke"
-  location = var.region
+resource "google_container_cluster" "gke_cluster" {
+  name               = "${var.environment}-gke-cluster"
+  location           = var.region
+  remove_default_node_pool = true
+  initial_node_count       = 1
 
-  networking_mode = "VPC_NATIVE"
-  network         = google_compute_network.vpc_network.id
-  subnetwork      = google_compute_subnetwork.subnet.id
+  network    = google_compute_network.vpc_network.name
+  subnetwork = google_compute_subnetwork.subnet.name
 
   ip_allocation_policy {
     cluster_secondary_range_name  = "pods"
     services_secondary_range_name = "services"
   }
 
-  master_authorized_networks_config {
-    cidr_blocks {
-      cidr_block   = var.master_cidr
-      display_name = "admin"
-    }
+  private_cluster_config {
+    enable_private_nodes    = true
+    enable_private_endpoint = false
+    master_ipv4_cidr_block  = var.master_cidr
   }
-
-  remove_default_node_pool = true
-  initial_node_count       = 1
 }
 
 resource "google_container_node_pool" "primary_nodes" {
-  name       = "${var.environment}-nodes"
+  name       = "primary-node-pool"
+  cluster    = google_container_cluster.gke_cluster.name
   location   = var.region
-  cluster    = google_container_cluster.primary.name
-  node_count = 1
 
   node_config {
     machine_type = "e2-medium"
-    oauth_scopes = ["https://www.googleapis.com/auth/cloud-platform"]
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
   }
-}
 
+  initial_node_count = 2
+}
