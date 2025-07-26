ad1491b1877534ccb61231491178e4d7570b29ff
Shauny123 <bywordofmouthcatering@gmail.com>
2025-07-26 00:25:33 -0600

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,44 @@
+# terraform/variables.tf
+variable "project_id" {
+  description = "GCP Project ID"
+  type        = string
+}
+
+variable "region" {
+  description = "GCP Region"
+  type        = string
+  default     = "us-central1"
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  validation {
+    condition     = contains(["development", "staging", "production"], var.environment)
+    error_message = "Environment must be one of: development, staging, production."
+  }
+}
+
+variable "subnet_cidr" {
+  description = "CIDR range for the subnet"
+  type        = string
+  default     = "10.0.0.0/24"
+}
+
+variable "pods_cidr" {
+  description = "CIDR range for pods"
+  type        = string
+  default     = "10.1.0.0/16"
+}
+
+variable "services_cidr" {
+  description = "CIDR range for services"
+  type        = string
+  default     = "10.2.0.0/16"
+}
+
+variable "master_cidr" {
+  description = "CIDR range for master"
+  type        = string
+  default     = "172.16.0.0/28"
+}
